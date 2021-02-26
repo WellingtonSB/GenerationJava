01d8a92bfea028d09e90be542e6abbee4d2a02ca
Wellington <71860661+WellingtonSB@users.noreply.github.com>
2021-02-26 17:56:32 -0300

    Programação sequencial 8
    O custo ao consumidor de um carro novo é a soma do custo de fábrica com a percentagem do distribuidor e dos impostos (aplicados ao custo de fábrica). Supondo que a percentagem do distribuidor seja de 28% e os impostos de 45%, escrever um sistema que leia o custo de fábrica de um carro e escreva o custo ao consumidor.

--- Turma18GAulas/src/ProgramaçãoSequencial/ProgramaçãoSequencial8.java
+++ Turma18GAulas/src/ProgramaçãoSequencial/ProgramaçãoSequencial8.java
@@ -1,14 +1,13 @@
-package ProgramaÁ„oSequencial;
-/*
-	O custo ao consumidor de um carro novo È a soma do custo de f·brica 
-	com a percentagem do distribuidor e dos impostos (aplicados ao custo de f·brica).
+package Programa√ß√£oSequencial;
+/*	O custo ao consumidor de um carro novo √© a soma do custo de f√°brica 
+	com a percentagem do distribuidor e dos impostos (aplicados ao custo de f√°brica).
 	Supondo que a percentagem do distribuidor seja de 28% e os impostos de 45%,
-	escrever um sistema que leia o custo de f·brica de um carro e escreva o custo ao consumidor. 
+	escrever um sistema que leia o custo de f√°brica de um carro e escreva o custo ao consumidor. 
 */
 
 import java.util.Scanner;
 
-public class ProgramaÁ„oSequencial8 {
+public class Programa√ß√£oSequencial8 {
 	public static void main(String[] args) {
 		
 		double custoFabrica, custoConsumidor;
@@ -21,7 +20,7 @@ public class ProgramaÁ„oSequencial8 {
 		custoConsumidor= custoFabrica+((custoFabrica*0.28)+(custoFabrica*0.45));
 		
 		
-		System.out.print("O custo do carro ao consumidor final È de R$: "+custoConsumidor);
+		System.out.print("O custo do carro ao consumidor final √© de R$: "+custoConsumidor);
 
 	}
 
