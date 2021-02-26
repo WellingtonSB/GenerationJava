8f295b0249e8a7499ed1b204633bd7084c7066e6
Wellington <71860661+WellingtonSB@users.noreply.github.com>
2021-02-26 17:48:24 -0300

    Laço de decisão 3
    /*Faça um programa que receba a idade de uma pessoa e mostre na saída em qual
    categoria ela se encontra:
     10-14 infantil
     15-17 juvenil
     18-25 adulto*/

--- Turma18GAulas/src/LacosDecisao/X3.java
+++ Turma18GAulas/src/LacosDecisao/X3.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 /*Faça um programa que receba a idade de uma pessoa e mostre na saída em qual
 categoria ela se encontra:
- 10-14 infantil
- 15-17 juvenil
- 18-25 adulto*/
+ 10-14 infantil
+ 15-17 juvenil
+ 18-25 adulto*/
 
 public class X3 {
 
