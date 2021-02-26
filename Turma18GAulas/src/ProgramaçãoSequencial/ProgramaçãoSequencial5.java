0a9c82dc4ce0c9e19c6b0b82665c3639359c9677
Wellington <71860661+WellingtonSB@users.noreply.github.com>
2021-02-26 17:54:33 -0300

    Programação Sequencial  5
    Faça um sistema que leia as 3 notas de um aluno e calcule a média final deste
    aluno. Considerar que a média é ponderada e que o peso das notas é: 2,3 e 5,
    respectivamente.

--- Turma18GAulas/src/ProgramaçãoSequencial/ProgramaçãoSequencial5.java
+++ Turma18GAulas/src/ProgramaçãoSequencial/ProgramaçãoSequencial5.java
@@ -1,31 +1,30 @@
-package ProgramaçãoSequencial;
+package ProgramaÃ§Ã£oSequencial;
 
 /*
-
-
-	Faça um sistema que leia as 3 notas de um aluno e calcule a média final deste aluno. 
-	Considerar que a média é ponderada e que o peso das notas é: 2,3 e 5, respectivamente.
+FaÃ§a um sistema que leia as 3 notas de um aluno e calcule a mÃ©dia final deste
+aluno. Considerar que a mÃ©dia Ã© ponderada e que o peso das notas Ã©: 2,3 e 5,
+respectivamente.
 */
 import java.util.Scanner;
 
-public class ProgramaçãoSequencial5 {
+public class ProgramaÃ§Ã£oSequencial5 {
 	public static void main(String[] args) {
 		double nota1, nota2, nota3, mediaPonderada;
 
 		Scanner ler = new Scanner(System.in);
-		System.out.print("Informe a 1°nota: ");
+		System.out.print("Informe a 1Â°nota: ");
 		nota1 = ler.nextDouble();
-		System.out.print("Informe a 2°nota: ");
+		System.out.print("Informe a 2Â°nota: ");
 		nota2 = ler.nextDouble();
-		System.out.print("informe a 3°nota: ");
+		System.out.print("informe a 3Â°nota: ");
 		nota3 = ler.nextDouble();
 
 		mediaPonderada = ((nota1 * 2) + (nota2 * 3) + (nota3 * 5)) / (2 + 3 + 5);
 
-		System.out.println("A sua média é de: " + mediaPonderada + "\n");
+		System.out.println("A sua mÃ©dia Ã© de: " + mediaPonderada + "\n");
 
 		if (mediaPonderada >= 6) {
-			System.out.println("Você foi aprovado, parabens!");
+			System.out.println("VocÃª foi aprovado, parabens!");
 		} else {
 			System.out.println("Voce foi reprovado.");
 		}
